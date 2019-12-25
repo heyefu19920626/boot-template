168f6139deafeb114313ab059f2cfede75af415a
heyefu <527380349@qq.com>
2019-12-25 18:06:51 +0800

    init

---
+++ src/main/java/com/heyefu/template/BootTemplateApplication.java
@@ -0,0 +1,16 @@
+package com.heyefu.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author heyefu
+ */
+@SpringBootApplication
+public class BootTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heyefu/template/BootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyefu.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
